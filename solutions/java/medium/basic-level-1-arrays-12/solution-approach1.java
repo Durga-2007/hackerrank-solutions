4ca8d7dd704dd507a4ea78ec30ff2a1f915a0705
Durga S <durgaselvi8060@gmail.com>
2026-08-24 20:42:17 +0530

    ✅ Basic_Level_1_Arrays_12 [java] — approach1

---
+++ solutions/java/medium/basic-level-1-arrays-12/solution-approach1.java
@@ -0,0 +1,47 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/seed-it-sde-c-level-0-1-arrays/challenges/basic-level-1-arrays-12/problem?isFullScreen=true
+// Problem     Basic_Level_1_Arrays_12
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-24, 08:42 p.m.
+// ──────────────────────────────────────────────────
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] == arr[j]) {
+
+                    int count = j - i + 1;
+
+                    if (count > max) {
+                        max = count;
+                    }
+                }
+            }
+        }
+
+        System.out.print(max);
+    }
+}
