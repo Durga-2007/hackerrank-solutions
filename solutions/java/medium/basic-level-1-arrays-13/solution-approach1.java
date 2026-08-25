e23a66cee713d1f725cb1669ebeba7bd8dbff1ae
Durga S <durgaselvi8060@gmail.com>
2026-08-25 17:53:07 +0530

    ✅ Basic_Level_1_Arrays_13 [java] — approach1

---
+++ solutions/java/medium/basic-level-1-arrays-13/solution-approach1.java
@@ -0,0 +1,43 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/seed-it-sde-c-level-0-1-arrays/challenges/basic-level-1-arrays-13/problem?isFullScreen=true
+// Problem     Basic_Level_1_Arrays_13
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-08-25, 05:53 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int max = 0;
+        int val =0;
+        for(int i=0;i<n;i++){
+            int count =0;
+            for(int j=0;j<n;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(max<count){
+                max = count;
+                val = arr[i];
+            }
+        }
+        System.out.print(val);
+    }
+}
