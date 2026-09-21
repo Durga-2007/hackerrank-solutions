ccf2294075beb4300ee801d3dc3761f39432e51e
Durga S <durgaselvi8060@gmail.com>
2026-09-21 19:24:00 +0530

    ✅ Basic_level_0_Strings_16 [java] — approach1

---
+++ solutions/java/medium/basic-level-0-strings-16/solution-approach1.java
@@ -0,0 +1,27 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/seed-it-sde-c-level-0-1-strings/challenges/basic-level-0-strings-16/problem?isFullScreen=true
+// Problem     Basic_level_0_Strings_16
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-21, 07:23 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        for(int i=0;i<str.length();i++){
+            System.out.print("*");
+        }
+    }
+}
