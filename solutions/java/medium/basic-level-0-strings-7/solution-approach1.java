e6dfed821cf76089f98e947bb7fa299500e84241
Durga S <durgaselvi8060@gmail.com>
2026-09-21 18:44:53 +0530

    ✅ Basic_level_0_Strings_7 [java] — approach1

---
+++ solutions/java/medium/basic-level-0-strings-7/solution-approach1.java
@@ -0,0 +1,26 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/seed-it-sde-c-level-0-1-strings/challenges/basic-level-0-strings-7/problem?isFullScreen=true
+// Problem     Basic_level_0_Strings_7
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-21, 06:44 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        
+        System.out.print(str.toString());
+    }
+}
