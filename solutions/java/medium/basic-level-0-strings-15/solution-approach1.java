d50db3628eaee1345e3b0694a6f428430fada1d2
Durga S <durgaselvi8060@gmail.com>
2026-09-23 18:54:10 +0530

    ✅ Basic_level_0_Strings_15 [java] — approach1

---
+++ solutions/java/medium/basic-level-0-strings-15/solution-approach1.java
@@ -0,0 +1,28 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/seed-it-sde-c-level-0-1-strings/challenges/basic-level-0-strings-15/problem?isFullScreen=true
+// Problem     Basic_level_0_Strings_15
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-23, 06:54 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        n = n % str.length();
+        String result = str.substring(n) + str.substring(0,n);
+        System.out.print(result);
+    }
+}
