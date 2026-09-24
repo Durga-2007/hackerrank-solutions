b80d5189089eb267ac6891247110ad084d86870f
Durga S <durgaselvi8060@gmail.com>
2026-09-24 18:41:31 +0530

    ✅ Basic_level_1_Strings_4 [java] — approach1

---
+++ solutions/java/medium/basic-level-1-strings-4/solution-approach1.java
@@ -0,0 +1,30 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/seed-it-sde-c-level-0-1-strings/challenges/basic-level-1-strings-4/problem?isFullScreen=true
+// Problem     Basic_level_1_Strings_4
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-24, 06:41 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        if(str1.equals(str2)){
+            System.out.print("Yes");
+        }else{
+            System.out.print("No");
+        }
+    }
+}
