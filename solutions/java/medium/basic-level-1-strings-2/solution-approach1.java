1dd72462086142eee0af33fda5a78870f4bf1578
Durga S <durgaselvi8060@gmail.com>
2026-09-24 18:24:19 +0530

    ✅ Basic_level_1_Strings_2 [java] — approach1

---
+++ solutions/java/medium/basic-level-1-strings-2/solution-approach1.java
@@ -0,0 +1,26 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/contests/seed-it-sde-c-level-0-1-strings/challenges/basic-level-1-strings-2/problem?isFullScreen=true
+// Problem     Basic_level_1_Strings_2
+// Difficulty  Medium
+// Subdomain   N/A
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-24, 06:24 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        
+        System.out.print(str.toString());
+    }
+}
